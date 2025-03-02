eecb8a326064b495339525f0dbaf3f48678c84e5
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-02 19:02:12 -0300

    test: adiciona TesteFrete e TestePedido e altera TestePedido para o novo padrao

--- src/teste/integracao/TesteDesconto.java
+++ src/teste/integracao/TesteDesconto.java
@@ -2,8 +2,10 @@ package teste.integracao;
 
 import dominio.Cliente.Cliente;
 import dominio.Cliente.ClientePessoaFisica;
-import dominio.Cliente.ClientePessoaJuridica;
 import dominio.Cliente.ServicoCliente;
+import dominio.Desconto.Factory.DescontoPedidoFactory;
+import dominio.Desconto.Factory.DescontoProdutoFactory;
+import dominio.Desconto.ServicoDesconto;
 import dominio.Notificacao.Notificador;
 import dominio.Notificacao.NotificadorEmail;
 import dominio.Pagamento.ServicoPagamento;
@@ -16,15 +18,12 @@ import repositorio.cliente.interfaces.compostas.ClienteAtivo;
 import repositorio.produto.RepositorioProdutoAtivo;
 import repositorio.produto.interfaces.compostas.ProdutoAtivo;
 
-import java.sql.SQLOutput;
-
-public class TestePedido {
+public class TesteDesconto {
     public static void main(String[] args) {
-
         Cliente cliente = new ClientePessoaFisica("Cliente 1","12345678910","cliente1@gmail.com");
 
         Produto produto = new ProdutoEletronico("Tv 1",1080);
-        Produto produto2 = new ProdutoPapelaria("caderno 1",29.99);
+        Produto produto2 = new ProdutoVestuario("Calça 1",70.50);
 
         ProdutoAtivo repositorioProduto = new RepositorioProdutoAtivo();
         ServicoProduto validaProduto = new ServicoProduto(repositorioProduto);
@@ -43,38 +42,16 @@ public class TestePedido {
         System.out.println("LISTAGEM DE PRODUTOS NO REPOSITORIO");
         System.out.println(repositorioProduto.listarProdutos());
 
-        System.out.println("MOSTRANDO PEDIDO SEM PRODUTOS ADICIONADOS");
         Pedido pedido = new PedidoPadrao(repositorioCliente.buscarPorId(1));
+        pedido.adicionarItem(produto,2,1080);
+        pedido.adicionarItem(produto2,5,75);
+
+        ServicoDesconto calculaDesconto = new ServicoDesconto(DescontoPedidoFactory.getListaDescontoPedido(),DescontoProdutoFactory.getListaDescontoProduto());
+
+        System.out.println("VALOR DE DESCONTO DO PEDIDO 1");
         System.out.println(pedido);
+        System.out.println(calculaDesconto.calcularMelhorDesconto(pedido));
 
-        Notificador notificarEmail = new NotificadorEmail();
-        ServicoPedido servicoPedido = new ServicoPedido(notificarEmail,pedido);
-        ServicoPagamento servicoPagamento = new ServicoPagamento(notificarEmail,pedido);
-
-        System.out.println("MOSTRANDO PEDIDO COM PRODUTOS ADICIONADOS");
-        pedido.adicionarItem(repositorioProduto.buscarPorId(1),2,1100);
-        pedido.adicionarItem(repositorioProduto.buscarPorId(2),3,35);
-        System.out.println(pedido);
-
-        System.out.println("REMOVENDO PRODUTO 1");
-        pedido.removerItem(repositorioProduto.buscarPorId(1));
-        System.out.println(pedido);
-
-        System.out.println("ALTERANDO QUANTIDADE");
-        pedido.alterarQuantidadeItem(repositorioProduto.buscarPorId(2),5);
-        System.out.println(pedido);
-
-        System.out.println("CONCLUINDO O PEDIDO");
-        servicoPedido.concluirPedido();
-        System.out.println(pedido);
-
-        System.out.println("ALTERANDO QUANTIDADE APOS CONCLUIR PEDIDO");
-        pedido.alterarQuantidadeItem(repositorioProduto.buscarPorId(2),3);
-        System.out.println("\n"+pedido);
-
-        System.out.println("PROCESSANDO O PAGAMENTO");
-        servicoPagamento.processarPagamento();
-        System.out.println(pedido);
 
     }
 }
