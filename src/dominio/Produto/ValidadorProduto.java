6a742c012921bb762e0f43cacb09e56b1f0c45f5
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-02 19:01:03 -0300

    metodo alterarNome removido, deixar para implementações futuras.

--- src/dominio/Produto/ValidadorProduto.java
+++ src/dominio/Produto/ValidadorProduto.java
@@ -4,15 +4,7 @@ import java.math.BigDecimal;
 
 public class ValidadorProduto {
     public static boolean validar(Produto produto) {
-        return validarNome(produto.getNome()) && validarValor(produto.getValorProduto());
-    }
-
-    private static boolean validarNome(String nome) {
-        if (nome == null || nome.trim().isEmpty()) {
-            return false;
-        }
-
-        return nome.matches("^(?=.*[A-Za-z])[A-Za-z0-9 ]+$");
+        return validarValor(produto.getValorProduto());
     }
 
     private static boolean validarValor(double valor) {
