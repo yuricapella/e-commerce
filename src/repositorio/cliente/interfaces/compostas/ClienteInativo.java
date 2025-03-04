24868fc20aa39c688daa30047944f90a5bb768d1
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-04 12:41:38 -0300

    feat: refatora repositorio cliente

--- src/repositorio/cliente/interfaces/compostas/ClienteInativo.java
+++
@@ -1,7 +0,0 @@
-package repositorio.cliente.interfaces.compostas;
-
-import repositorio.cliente.interfaces.especificas.ClienteManipulacao;
-import repositorio.cliente.interfaces.especificas.ClienteConsulta;
-
-public interface ClienteInativo extends ClienteConsulta, ClienteManipulacao {
-}
