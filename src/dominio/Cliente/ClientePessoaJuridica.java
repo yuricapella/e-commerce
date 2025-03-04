2d01b5c818f73071cc4a96a8c951f54d8933eb57
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-04 20:01:46 -0300

    feat: refatora cliente e suas classes

--- src/dominio/Cliente/ClientePessoaJuridica.java
+++ src/dominio/Cliente/ClientePessoaJuridica.java
@@ -1,12 +1,23 @@
 package dominio.Cliente;
 
-public class ClientePessoaJuridica extends Cliente{
+public class ClientePessoaJuridica extends Cliente {
     public ClientePessoaJuridica(String nome, String documento, String email) {
         super(nome, documento, email, TipoCliente.PESSOA_JURIDICA);
+        if (!validarDocumento()) {
+            throw new IllegalArgumentException("CNPJ inválido.");
+        }
     }
 
     @Override
     public boolean validarDocumento() {
-        return ValidadorDocumento.validarCNPJ(documento);
+        return ValidaCliente.validarCNPJ(documento);
+    }
+
+    @Override
+    public String toString() {
+        String clienteToString = super.toString();
+        String documentoFormatado = FormataDocumento.formatarCNPJ(this.documento);
+        clienteToString = clienteToString.replace(this.documento, documentoFormatado);
+        return clienteToString;
     }
 }
--- src/dominio/Cliente/ServicoCliente.java
+++ src/dominio/Cliente/ServicoCliente.java
@@ -1,20 +1,43 @@
 package dominio.Cliente;
 
-import repositorio.cliente.interfaces.compostas.ClienteAtivo;
+import repositorio.cliente.interfaces.compostas.RepositorioCliente;
 
 public class ServicoCliente {
-    private final ClienteAtivo clienteAtivo;
+    private final RepositorioCliente repositorioCliente;
+    private final AlteradorCliente alteradorCliente;
 
-    public ServicoCliente(ClienteAtivo clienteAtivo) {
-        this.clienteAtivo = clienteAtivo;
+    public ServicoCliente(RepositorioCliente repositorioCliente, AlteradorCliente alteradorCliente) {
+        this.repositorioCliente = repositorioCliente;
+        this.alteradorCliente = alteradorCliente;
     }
 
     public void adicionarCliente(Cliente cliente) {
+        if (repositorioCliente.buscarPorId(cliente.getId()) != null) {
+            System.out.println("Cliente já está no repositório.");
+            return;
+        }
+
         if (cliente.validarDocumento()) {
-            clienteAtivo.adicionar(cliente);
+            repositorioCliente.adicionar(cliente);
         } else {
             throw new IllegalArgumentException("Documento inválido.");
         }
     }
 
+    public void atualizarNomeCliente(Cliente cliente, String novoNome) {
+        alteradorCliente.alterarNome(cliente, novoNome);
+    }
+
+    public void atualizarDocumentoCliente(Cliente cliente, String novoDocumento) {
+        if (cliente.validarDocumento()) {
+            alteradorCliente.alterarDocumento(cliente, novoDocumento);
+        } else {
+            throw new IllegalArgumentException("Documento inválido.");
+        }
+    }
+
+    public void atualizarEmailCliente(Cliente cliente, String novoEmail) {
+        alteradorCliente.alterarEmail(cliente, novoEmail);
+    }
 }
+
