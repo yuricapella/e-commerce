3aafc91e3caa333f8e8a65af04b670afcb28d6b7
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-05 11:13:52 -0300

    feat: adiciona repositorio usuario

---
+++ src/repositorio/usuario/interfaces/compostas/RepositorioUsuario.java
@@ -0,0 +1,8 @@
+package repositorio.usuario.interfaces.compostas;
+
+import repositorio.usuario.interfaces.especificas.UsuarioConsulta;
+import repositorio.usuario.interfaces.especificas.UsuarioManipulacao;
+
+public interface RepositorioUsuario extends UsuarioConsulta, UsuarioManipulacao{
+
+}
