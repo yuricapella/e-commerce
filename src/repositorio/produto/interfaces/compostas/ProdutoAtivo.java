3e9d932822cf00a73aa11f3fd8b2f8c60c892437
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-04 21:09:28 -0300

    feat: refatora repositorio produto

--- src/repositorio/produto/interfaces/compostas/ProdutoAtivo.java
+++
@@ -1,9 +0,0 @@
-package repositorio.produto.interfaces.compostas;
-
-import repositorio.produto.interfaces.especificas.ProdutoConsulta;
-import repositorio.produto.interfaces.especificas.ProdutoManipulacao;
-import repositorio.produto.interfaces.especificas.ProdutoAlteracao;
-
-public interface ProdutoAtivo extends ProdutoConsulta, ProdutoManipulacao, ProdutoAlteracao {
-
-}
--- src/repositorio/produto/interfaces/especificas/ProdutoAlteracao.java
+++
@@ -1,7 +0,0 @@
-package repositorio.produto.interfaces.especificas;
-
-import dominio.Produto.Produto;
-
-public interface ProdutoAlteracao {
-    void alterarNome(Produto produto, String novoNome);
-}
