f708dbd8b041de5147d0615843133e09b5f21849
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-06 15:11:35 -0300

    organiza menu admin para melhorar a visualização das consultas

--- src/aplicacao/menu/menuadmin/MenuAdmin.java
+++ src/aplicacao/menu/menuadmin/MenuAdmin.java
@@ -68,134 +68,218 @@ public class MenuAdmin {
     }
 
     private void exibirClientes() {
-        System.out.println("\n1. Todos os Clientes");
-        System.out.println("2. Clientes Ativos");
-        System.out.println("3. Clientes Inativos");
-        System.out.print("Escolha uma opção: ");
-        String escolha = scanner.nextLine();
-        switch (escolha) {
-            case "1":
-                System.out.println("\nTodos os Clientes:");
-                List<Cliente> todosClientes = repositorioCliente.listarTodosClientes();
-                for (Cliente cliente : todosClientes) {
-                    System.out.println(cliente); // Utiliza o toString() de Cliente
-                }
-                break;
-            case "2":
-                System.out.println("\nClientes Ativos:");
-                List<Cliente> clientesAtivos = repositorioCliente.listarClientesAtivos();
-                for (Cliente cliente : clientesAtivos) {
-                    System.out.println(cliente);
-                }
-                break;
-            case "3":
-                System.out.println("\nClientes Inativos:");
-                List<Cliente> clientesInativos = repositorioCliente.listarClientesInativos();
-                for (Cliente cliente : clientesInativos) {
-                    System.out.println(cliente);
-                }
-                break;
-            default:
-                System.out.println("Opção inválida.");
+        boolean menuAtivo = true;
+        while (menuAtivo) {
+            System.out.println("\n1. Todos os Clientes");
+            System.out.println("2. Clientes Ativos");
+            System.out.println("3. Clientes Inativos");
+            System.out.println("4. Voltar");
+            System.out.print("Escolha uma opção: ");
+            String escolha = scanner.nextLine();
+
+            switch (escolha) {
+                case "1":
+                    List<Cliente> todosClientes = repositorioCliente.listarTodosClientes();
+                    if (todosClientes.isEmpty()) {
+                        System.out.println("Nenhum cliente cadastrado.");
+                    } else {
+                        System.out.println("\nTodos os Clientes:");
+                        for (Cliente cliente : todosClientes) {
+                            System.out.println(cliente);
+                        }
+                    }
+                    break;
+                case "2":
+                    List<Cliente> clientesAtivos = repositorioCliente.listarClientesAtivos();
+                    if (clientesAtivos.isEmpty()) {
+                        System.out.println("Nenhum cliente ativo encontrado.");
+                    } else {
+                        System.out.println("\nClientes Ativos:");
+                        for (Cliente cliente : clientesAtivos) {
+                            System.out.println(cliente);
+                        }
+                    }
+                    break;
+                case "3":
+                    List<Cliente> clientesInativos = repositorioCliente.listarClientesInativos();
+                    if (clientesInativos.isEmpty()) {
+                        System.out.println("Nenhum cliente inativo encontrado.");
+                    } else {
+                        System.out.println("\nClientes Inativos:");
+                        for (Cliente cliente : clientesInativos) {
+                            System.out.println(cliente);
+                        }
+                    }
+                    break;
+                case "4":
+                    menuAtivo = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
         }
     }
+
 
     private void exibirProdutos() {
-        System.out.println("\n1. Todos os Produtos");
-        System.out.println("2. Produtos Ativos");
-        System.out.println("3. Produtos Inativos");
-        System.out.print("Escolha uma opção: ");
-        String escolha = scanner.nextLine();
-        switch (escolha) {
-            case "1":
-                System.out.println("\nTodos os Produtos:");
-                List<Produto> todosProdutos = repositorioProduto.listarTodosProdutos();
-                for (Produto produto : todosProdutos) {
-                    System.out.println(produto); // Utiliza o toString() de Produto
-                }
-                break;
-            case "2":
-                System.out.println("\nProdutos Ativos:");
-                List<Produto> produtosAtivos = repositorioProduto.listarProdutosAtivos();
-                for (Produto produto : produtosAtivos) {
-                    System.out.println(produto);
-                }
-                break;
-            case "3":
-                System.out.println("\nProdutos Inativos:");
-                List<Produto> produtosInativos = repositorioProduto.listarProdutosInativos();
-                for (Produto produto : produtosInativos) {
-                    System.out.println(produto);
-                }
-                break;
-            default:
-                System.out.println("Opção inválida.");
+        boolean menuAtivo = true;
+        while (menuAtivo) {
+            System.out.println("\n1. Todos os Produtos");
+            System.out.println("2. Produtos Ativos");
+            System.out.println("3. Produtos Inativos");
+            System.out.println("4. Voltar");
+            System.out.print("Escolha uma opção: ");
+            String escolha = scanner.nextLine();
+
+            switch (escolha) {
+                case "1":
+                    List<Produto> todosProdutos = repositorioProduto.listarTodosProdutos();
+                    if (todosProdutos.isEmpty()) {
+                        System.out.println("Nenhum produto cadastrado.");
+                    } else {
+                        System.out.println("\nTodos os Produtos:");
+                        for (Produto produto : todosProdutos) {
+                            System.out.println(produto);
+                        }
+                    }
+                    break;
+                case "2":
+                    List<Produto> produtosAtivos = repositorioProduto.listarProdutosAtivos();
+                    if (produtosAtivos.isEmpty()) {
+                        System.out.println("Nenhum produto ativo encontrado.");
+                    } else {
+                        System.out.println("\nProdutos Ativos:");
+                        for (Produto produto : produtosAtivos) {
+                            System.out.println(produto);
+                        }
+                    }
+                    break;
+                case "3":
+                    List<Produto> produtosInativos = repositorioProduto.listarProdutosInativos();
+                    if (produtosInativos.isEmpty()) {
+                        System.out.println("Nenhum produto inativo encontrado.");
+                    } else {
+                        System.out.println("\nProdutos Inativos:");
+                        for (Produto produto : produtosInativos) {
+                            System.out.println(produto);
+                        }
+                    }
+                    break;
+                case "4":
+                    menuAtivo = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
         }
     }
+
 
     private void exibirPedidos() {
-        System.out.println("\n1. Todos os Pedidos");
-        System.out.println("2. Pedidos em Andamento");
-        System.out.println("3. Pedidos Finalizados");
-        System.out.print("Escolha uma opção: ");
-        String escolha = scanner.nextLine();
-        switch (escolha) {
-            case "1":
-                System.out.println("\nTodos os Pedidos:");
-                List<Pedido> todosPedidos = repositorioPedido.listarTodosPedidos();
-                for (Pedido pedido : todosPedidos) {
-                    System.out.println(pedido); // Utiliza o toString() de Pedido
-                }
-                break;
-            case "2":
-                System.out.println("\nPedidos em Andamento:");
-                List<Pedido> pedidosAndamento = repositorioPedido.listarEmAndamento();
-                for (Pedido pedido : pedidosAndamento) {
-                    System.out.println(pedido);
-                }
-                break;
-            case "3":
-                System.out.println("\nPedidos Finalizados:");
-                List<Pedido> pedidosFinalizados = repositorioPedido.listarFinalizados();
-                for (Pedido pedido : pedidosFinalizados) {
-                    System.out.println(pedido);
-                }
-                break;
-            default:
-                System.out.println("Opção inválida.");
+        boolean menuAtivo = true;
+        while (menuAtivo) {
+            System.out.println("\n1. Todos os Pedidos");
+            System.out.println("2. Pedidos em Andamento");
+            System.out.println("3. Pedidos Finalizados");
+            System.out.println("4. Voltar");
+            System.out.print("Escolha uma opção: ");
+            String escolha = scanner.nextLine();
+
+            switch (escolha) {
+                case "1":
+                    List<Pedido> todosPedidos = repositorioPedido.listarTodosPedidos();
+                    if (todosPedidos.isEmpty()) {
+                        System.out.println("Nenhum pedido cadastrado.");
+                    } else {
+                        System.out.println("\nTodos os Pedidos:");
+                        for (Pedido pedido : todosPedidos) {
+                            System.out.println(pedido);
+                        }
+                    }
+                    break;
+                case "2":
+                    List<Pedido> pedidosAndamento = repositorioPedido.listarEmAndamento();
+                    if (pedidosAndamento.isEmpty()) {
+                        System.out.println("Nenhum pedido em andamento.");
+                    } else {
+                        System.out.println("\nPedidos em Andamento:");
+                        for (Pedido pedido : pedidosAndamento) {
+                            System.out.println(pedido);
+                        }
+                    }
+                    break;
+                case "3":
+                    List<Pedido> pedidosFinalizados = repositorioPedido.listarFinalizados();
+                    if (pedidosFinalizados.isEmpty()) {
+                        System.out.println("Nenhum pedido finalizado.");
+                    } else {
+                        System.out.println("\nPedidos Finalizados:");
+                        for (Pedido pedido : pedidosFinalizados) {
+                            System.out.println(pedido);
+                        }
+                    }
+                    break;
+                case "4":
+                    menuAtivo = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
         }
     }
 
+
     private void exibirUsuarios() {
-        System.out.println("\n1. Todos os Usuários");
-        System.out.println("2. Usuários Ativos");
-        System.out.println("3. Usuários Inativos");
-        System.out.print("Escolha uma opção: ");
-        String escolha = scanner.nextLine();
-        switch (escolha) {
-            case "1":
-                System.out.println("\nTodos os Usuários:");
-                List<Usuario> todosUsuarios = repositorioUsuario.listarTodosUsuarios();
-                for (Usuario usuario : todosUsuarios) {
-                    System.out.println(usuario); // Recomenda-se implementar toString() em Usuario
-                }
-                break;
-            case "2":
-                System.out.println("\nUsuários Ativos:");
-                List<Usuario> usuariosAtivos = repositorioUsuario.listarUsuariosAtivos();
-                for (Usuario usuario : usuariosAtivos) {
-                    System.out.println(usuario);
-                }
-                break;
-            case "3":
-                System.out.println("\nUsuários Inativos:");
-                List<Usuario> usuariosInativos = repositorioUsuario.listarUsuariosInativos();
-                for (Usuario usuario : usuariosInativos) {
-                    System.out.println(usuario);
-                }
-                break;
-            default:
-                System.out.println("Opção inválida.");
+        boolean menuAtivo = true;
+        while (menuAtivo) {
+            System.out.println("\n1. Todos os Usuários");
+            System.out.println("2. Usuários Ativos");
+            System.out.println("3. Usuários Inativos");
+            System.out.println("4. Voltar");
+            System.out.print("Escolha uma opção: ");
+            String escolha = scanner.nextLine();
+
+            switch (escolha) {
+                case "1":
+                    List<Usuario> todosUsuarios = repositorioUsuario.listarTodosUsuarios();
+                    if (todosUsuarios.isEmpty()) {
+                        System.out.println("Nenhum usuário cadastrado.");
+                    } else {
+                        System.out.println("\nTodos os Usuários:");
+                        for (Usuario usuario : todosUsuarios) {
+                            System.out.println(usuario);
+                        }
+                    }
+                    break;
+                case "2":
+                    List<Usuario> usuariosAtivos = repositorioUsuario.listarUsuariosAtivos();
+                    if (usuariosAtivos.isEmpty()) {
+                        System.out.println("Nenhum usuário ativo encontrado.");
+                    } else {
+                        System.out.println("\nUsuários Ativos:");
+                        for (Usuario usuario : usuariosAtivos) {
+                            System.out.println(usuario);
+                        }
+                    }
+                    break;
+                case "3":
+                    List<Usuario> usuariosInativos = repositorioUsuario.listarUsuariosInativos();
+                    if (usuariosInativos.isEmpty()) {
+                        System.out.println("Nenhum usuário inativo encontrado.");
+                    } else {
+                        System.out.println("\nUsuários Inativos:");
+                        for (Usuario usuario : usuariosInativos) {
+                            System.out.println(usuario);
+                        }
+                    }
+                    break;
+                case "4":
+                    menuAtivo = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
         }
     }
+
 }
