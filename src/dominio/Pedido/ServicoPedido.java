31818236d885da78ad890d08d7748857fdde1a82
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-06 00:41:20 -0300

    feat: adiciona notificador e valor total com descontos e frete ao servicopedido para processar pagamento corretamente.

--- src/dominio/Pedido/ServicoPedido.java
+++ src/dominio/Pedido/ServicoPedido.java
@@ -2,27 +2,35 @@ package dominio.Pedido;
 
 import dominio.Pagamento.Pagamento;
 import dominio.Pagamento.PagamentoPix;
+import dominio.Notificacao.ServicoNotificador;
 
 public class ServicoPedido {
+    private final ServicoNotificador servicoNotificador;
+
+    public ServicoPedido(ServicoNotificador servicoNotificador) {
+        this.servicoNotificador = servicoNotificador;
+    }
 
     public void concluirPedido(Pedido pedido) {
         if (pedido.getItens().isEmpty() || pedido.getValorTotal() <= 0 || pedido.getStatus() != PedidoStatus.ABERTO) {
             throw new IllegalStateException("Pedido deve ter ao menos um item, valor > 0 e estar aberto.");
         }
         pedido.setStatus(PedidoStatus.AGUARDANDO_PAGAMENTO);
+        servicoNotificador.notificarMudancaDeStatus(pedido);
     }
 
-    public void processarPagamento(Pedido pedido) {
+    public void processarPagamento(Pedido pedido, double valorPagamento) {
         if (pedido.getStatus() != PedidoStatus.AGUARDANDO_PAGAMENTO) {
             throw new IllegalStateException("Pedido não está aguardando pagamento.");
         }
 
         Pagamento pagamento = new PagamentoPix();
-        boolean pagamentoRealizado = pagamento.realizarPagamento(pedido.getValorTotal());
+        boolean pagamentoRealizado = pagamento.realizarPagamento(valorPagamento);
 
         if (pagamentoRealizado) {
             pedido.setStatus(PedidoStatus.PAGO);
             System.out.println("Pagamento realizado com sucesso!");
+            servicoNotificador.notificarMudancaDeStatus(pedido);
         } else {
             throw new IllegalStateException("Erro ao processar pagamento.");
         }
@@ -32,7 +40,15 @@ public class ServicoPedido {
         if (pedido.getStatus() != PedidoStatus.PAGO) {
             throw new IllegalStateException("Pedido ainda não foi pago!");
         }
+
+        realizarEntrega(pedido);
+
         pedido.setStatus(PedidoStatus.FINALIZADO);
-        System.out.println("Pedido finalizado.");
+        System.out.println("Pedido finalizado e entregue.");
+        servicoNotificador.notificarMudancaDeStatus(pedido);
+    }
+
+    private void realizarEntrega(Pedido pedido) {
+        System.out.println("Iniciando processo de entrega...");
     }
 }
